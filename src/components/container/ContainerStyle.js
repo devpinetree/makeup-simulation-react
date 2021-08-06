bbdebe73705a0e57ba27007192d4daa387937700
devpinetree <bosollee@gmail.com>
2021-08-06 19:54:10 +0900

    ADD: 전체 컨테이너 스타일, 색상 스타일 추가

---
+++ src/components/container/ContainerStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const WrapperContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/styles/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  black: '#333333',
+  brownishGrey: '#666666',
+  white: '#ffffff',
+  GreyishWhite: '#f8f8f8',
+  brightYellow: '#faf400',
+};
+export default colors;
